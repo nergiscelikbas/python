b10109e360d979df8f5d7734539c31651676e95d
nergiscelikbas <nergiscelikbas0@gmail.com>
2024-12-20 11:50:31 +0300

    Add files via upload

---
+++ 241220/ornek1.py
@@ -0,0 +1,7 @@
+not1=int(input("1.sınav notu:"))
+not2=int(input("2.sınav notu:"))
+not3=int(input("3.sınav notu:"))
+
+ortalama=(not1+not2+not3)/2
+
+print("sınav notlarının ortalaması:",ortalama)
---
+++ 241220/ornek2.py
@@ -0,0 +1,8 @@
+kisa_kenar=int(input("kısa kenarı gir:"))
+uzun_kenar=int(input("uzun kenarı gir:"))
+
+cevre=(kisa_kenar+uzun_kenar)*2
+alan=kisa_kenar*uzun_kenar
+
+print("dikdörtgenin çevresi:",cevre)
+print("dikdörtgenin alanı:",alan)
