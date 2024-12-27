77791c4e6adaf478fdab467093da5c0e28ce7169
nergiscelikbas <nergiscelikbas0@gmail.com>
2024-12-27 12:04:49 +0300

    Add files via upload

---
+++ 241227/ornek1.py
@@ -0,0 +1,5 @@
+birim_fiyat=20
+metre_kup=int(input("tüketilen su miktarı:"))
+
+islem=(birim_fiyat*metre_kup)
+print("faturanız bu kadardır:", islem)
---
+++ 241227/ornek2.py
@@ -0,0 +1,6 @@
+yazili_notu1=int(input("1. yazılı notu:"))
+yazili_notu2=int(input("2. yazılı notu:"))
+performan_notu1=int(input("1. performans notu:"))
+performan_notu2=int(input("2. performans notu:"))
+ortalama=(yazili_notu1+yazili_notu2+performan_notu1+performan_notu2)/4
+print("notlarınızın ortalaması:",ortalama)
